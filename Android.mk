aa2b3ee823e4fe9f1be07d312639aa04793874e4
Mikael Magnusson <mikma@users.sourceforge.net>
2013-09-08 23:18:26 +0200

    Add initial android commit

---
+++ Android.mk
@@ -0,0 +1,46 @@
+
+TOP_DIR := $(call my-dir)
+LOCAL_PATH := $(TOP_DIR)/src
+
+common_src_include := $(LOCAL_PATH)/common
+
+#
+# Static libcommon library
+#
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := libcommon
+LOCAL_CFLAGS	:= -DHAVE_CONFIG_H
+LOCAL_C_INCLUDES := \
+	jni
+LOCAL_SRC_FILES := \
+	common/network.c \
+	common/message.c \
+	common/openssl.c \
+
+LOCAL_SHARED_LIBRARIES := libssl libcrypto
+
+include $(BUILD_STATIC_LIBRARY)
+
+
+#
+# Shared libpcsclite library
+#
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := libpcsclite
+LOCAL_CFLAGS	:= -DHAVE_CONFIG_H
+LOCAL_C_INCLUDES := \
+	jni \
+	$(common_src_include)
+
+LOCAL_SRC_FILES := \
+	client/client.c
+
+LOCAL_LDLIBS	:= -llog
+
+LOCAL_SHARED_LIBRARIES := libcommon libssl libcrypto
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,openssl)
---
+++ jni/Application.mk
@@ -0,0 +1,5 @@
+APP_PLATFORM=android-8
+# TODO add mips x86
+APP_ABI := armeabi
+APP_PROJECT_PATH := .
+APP_BUILD_SCRIPT := $(APP_PROJECT_PATH)/Android.mk
