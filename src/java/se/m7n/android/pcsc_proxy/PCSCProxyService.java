24537fb90b4aa9a40cb26236f5dae55c1e7d30e6
Mikael Magnusson <mikma@users.sourceforge.net>
2013-09-28 21:41:14 +0200

    Add unix domain socket server

--- src/java/se/m7n/android/pcsc_proxy/PCSCProxyService.java
+++ src/java/se/m7n/android/pcsc_proxy/PCSCProxyService.java
@@ -8,6 +8,10 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
+import android.net.Credentials;
+import android.net.LocalServerSocket;
+import android.net.LocalSocket;
+
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -19,17 +23,25 @@ import java.util.*;
 import se.m7n.android.env.Setenv;
 
 public class PCSCProxyService extends Service {
-    public static final String TAG = "pcsc-proxy";
+    public static final String TAG = "PCSCProxyService";
 
     private final Lock pkcs11Lock = new ReentrantLock();
     private final Condition pkcs11Bound = pkcs11Lock.newCondition();
+
+    private String mSocketName;
+    private Thread mThread;
+    private LocalServerSocket mServer;
+    private boolean mStopping = false;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        Log.d(TAG, "onCreate");
+        mSocketName = toString();
+
+        Log.d(TAG, "onCreate: " + mSocketName);
         Setenv.setenv("test", "value", 1);
+        start();
     }
 
     @Override
@@ -37,6 +49,7 @@ public class PCSCProxyService extends Service {
         super.onDestroy();
 
         Log.d(TAG, "onDestroy");
+        stop();
     }
 
     @Override
@@ -45,6 +58,52 @@ public class PCSCProxyService extends Service {
 
         Log.d(TAG, "onBind");
         return mBinder;
+    }
+
+    private void stop() {
+        try {
+            mStopping = true;
+            LocalSocket mSock = new LocalSocket();
+            mSock.connect(mServer.getLocalSocketAddress());
+            mSock.close();
+        } catch(IOException e) {
+            Log.d(TAG, "Stop", e);
+        }
+    }
+
+    private void start() {
+        mStopping = false;
+        mThread = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        mServer = new LocalServerSocket(mSocketName);
+
+                        Log.i(TAG, "Waiting for connection on: " + mServer);
+
+                        LocalSocket socket;
+
+                        while ((socket = mServer.accept()) != null) {
+                            Credentials creds = socket.getPeerCredentials();
+
+                            Log.i(TAG, "Accepted: " + socket);
+                            Log.i(TAG, "Credentials: " + creds);
+                            socket.close();
+
+                            if (mStopping) {
+                                Log.i(TAG, "Stopped");
+                                mServer.close();
+                                mServer = null;
+                                break;
+                            }
+                        }
+                    } catch(IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    Log.i(TAG, "Thread end");
+                    mThread = null;
+                }
+            });
+        mThread.start();
     }
 
     private final IBinder mBinder = new PCSCProxyBinder();
